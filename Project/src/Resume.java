4ed64df7b48f90d8848a60239319f2359887d1b3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-07 22:36:56 +0530

    Resume Builder

--- Project/src/Resume.java
+++ Project/src/Resume.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Resume extends JFrame implements ActionListener {
@@ -8,5 +10,9 @@ public class Resume extends JFrame implements ActionListener {
 
     public static void main(String[] args) throws Exception {
         new Resume();
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        
     }
 }
