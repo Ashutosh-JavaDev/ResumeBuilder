b44f651611a9ffd497f950dca60795bfe0db760e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-08 10:07:21 +0530

    Resume Builder

--- Project/src/Resume.java
+++ Project/src/Resume.java
@@ -5,6 +5,11 @@ import java.awt.event.ActionListener;
 
 public class Resume extends JFrame implements ActionListener {
     Resume() {
+        setTitle("Resume Builder");
+        setSize(500,800);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
 
     }
 
