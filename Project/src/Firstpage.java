304ed5df46f0756c265b27c26bbc56765476f180
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-12 21:42:24 +0530

    Resume Builder

--- Project/src/Firstpage.java
+++ Project/src/Firstpage.java
@@ -26,7 +26,7 @@ public class Firstpage extends JFrame implements ActionListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(400, 125);
         newPanel();
-        // LabelPanel();
+        SecondPanel();
         // FieldPanel();
         // ButtonFunction();
         // labelFunction();
