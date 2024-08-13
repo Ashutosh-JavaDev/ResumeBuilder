617b254abc548fcd6671d70e444e0cd8d029118c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-13 15:53:26 +0530

    Resume Builder

--- Project/src/Firstpage.java
+++ Project/src/Firstpage.java
@@ -25,7 +25,7 @@ public class Firstpage extends JFrame implements ActionListener {
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(400, 125);
-        newPanel();
+        new MainPanel();
         SecondPanel();
         ThirdPanel();
         Fourthpanel();
@@ -35,21 +35,6 @@ public class Firstpage extends JFrame implements ActionListener {
     }
 
     // new Panel
-    public void newPanel() {
-        firstPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.setColor(new Color(0, 204, 204, 50));
-                g.fillRect(0, 0, getWidth(), getHeight());
-            }
-        };
-        firstPanel.setSize(990, 590);
-        firstPanel.setLayout(null);
-        firstPanel.setBounds(5, 5, 990, 590);
-        firstPanel.setOpaque(false);
-        add(firstPanel);
-    }
 
     public void SecondPanel() {
         secondPanel = new JPanel() {
---
+++ Project/src/MainPanel.java
@@ -0,0 +1,22 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+public class MainPanel extends JFrame {
+    JPanel firstPanel;
+       public void newPanel() {
+        firstPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(new Color(0, 204, 204, 50));
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        firstPanel.setSize(990, 590);
+        firstPanel.setLayout(null);
+        firstPanel.setBounds(5, 5, 990, 590);
+        firstPanel.setOpaque(false);
+        add(firstPanel);
+    }
+}
