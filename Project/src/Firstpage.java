fd9f363f1f10e94e68ec6a1a1909765258cdebb9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-13 16:39:18 +0530

    Resume Builder

--- Project/src/Firstpage.java
+++ Project/src/Firstpage.java
@@ -27,23 +27,7 @@ public class Firstpage extends JFrame implements ActionListener {
         // fieldFunction();
         setVisible(true);
     }
-    public void ThirdPanel() {
-        thirdPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.setColor(new Color(250,14,14, 100));
-                g.fillRect(0, 0, getWidth(), getHeight());
-            }
-        };
-        thirdPanel.setSize(440, 270);
-        thirdPanel.setLayout(null);
-        thirdPanel.setBounds(5,280,440,265);
-        thirdPanel.setOpaque(false);
-        firstPanel.add(thirdPanel);
-        firstPanel.revalidate();
-        firstPanel.repaint();
-    }
+
     // 
     public void Fourthpanel() {
         forthPanel = new JPanel() {
--- Project/src/SecondPanel.java
+++ Project/src/SecondPanel.java
@@ -1,3 +1,26 @@
-public class SecondPanel {
-    
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class SecondPanel extends JFrame {
+    JPanel thirdPanel;
+
+    public void ThirdPanel() {
+        thirdPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(new Color(250, 14, 14, 100));
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        thirdPanel.setSize(440, 270);
+        thirdPanel.setLayout(null);
+        thirdPanel.setBounds(5, 280, 440, 265);
+        thirdPanel.setOpaque(false);
+        firstPanel.add(thirdPanel);
+        firstPanel.revalidate();
+        firstPanel.repaint();
+    }
 }
